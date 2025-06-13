0980912aa6498dced6748552dfc7600935a1dc58
zitro <ajosueramos@gmail.com>
2025-06-13 18:24:52 -0500

    project finish

--- frontend/auth-client/src/App.jsx
+++ frontend/auth-client/src/App.jsx
@@ -1,28 +1,25 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterForm from './components/RegisterForm';
 import LoginForm from './components/LoginForm';
 import ProtectedPage from './components/ProtectedPage';
 import GoogleSuccess from './pages/GoogleSuccess';
 import UserProfile from './components/UserProfile';
-import ProfilePage from './components/ProfilePage';
 import Navbar from './components/Navbar';
 import { AuthProvider, useAuth } from './context/AuthContext';
 
 function AppContent() {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn } = useAuth(); // Obtiene el estado de login
 
   return (
     <>
       <Navbar />
       <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-        {isLoggedIn && <UserProfile />}
         <Routes>
           <Route path="/" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
-          <Route path="/protected" element={<ProtectedPage />} />
+          <Route path="/protected" element={isLoggedIn ? <ProtectedPage /> : <LoginForm />} />
           <Route path="/google-success" element={<GoogleSuccess />} />
-          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/profile" element={isLoggedIn ? <UserProfile /> : <LoginForm />} />
         </Routes>
       </div>
     </>
